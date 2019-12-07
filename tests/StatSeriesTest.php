6a8aabc07a316d8da2fe210994376883bf919627
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2019-12-07 19:22:46 +0200

    initialization phpunit testing

---
+++ tests/StatSeriesTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace swe\tests;
+
+use PHPUnit\Framework\TestCase;
+use swe\StatSeries;
+
+/**
+ * Description of StatSeriesTest
+ *
+ * @author Vyacheslav
+ */
+class StatSeriesTest extends TestCase
+{
+    //put your code here
+}
